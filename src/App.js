7b149797d088ad40dafb04fb8a94518fb1ac6a5d
Dmitry Birin <dmitry.birin@gmail.com>
2018-10-05 23:11:15 +0300

    black background (SW, bro!)

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 import styled from 'styled-components'
 
+// import { characterList } from './models/Character'
+import './App.css'
+
 const Title = styled.h1`
 	font-size: 1.5em;
 	text-align: center;
-	color: blue;
-`
-const SubTitle = styled.h3`
-	font-size: 0.8em;
-	text-align: center;
+	color: yellow;
 `
 
 const App = () => {
 	return (
 		<div>
-			<Title>This is styled Title!</Title>
-			<SubTitle>Made with React</SubTitle>
+			<Title>
+				Star Wars Api with MobX
+				<strike>-Wing</strike>
+			</Title>
 		</div>
 	)
 }
