9e3253d0b30d8ab9b63b2828daf77caec66638e0
Dmitry Birin <dmitry.birin@gmail.com>
2018-10-07 01:29:01 +0300

    fix the question

--- src/components/GuessGame.js
+++ src/components/GuessGame.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import { observer } from 'mobx-react'
 import store from '../models'
+import styled from 'styled-components'
+
+const AnswerContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`
+
+const Input = styled.input`
+	width: 200px;
+`
 
 const GuessGame = () => {
 	const { game } = store
 	return (
-		<div>
-			<input
+		<AnswerContainer>
+			<h2>{`What height ${game.character.name} is?`}</h2>
+			<Input
 				type="number"
 				disabled={game.showAnswer}
 				value={game.guessed || ''}
@@ -14,13 +27,13 @@ const GuessGame = () => {
 			/>
 			<button onClick={() => game.checkGuess()}>Guess!</button>
 			{game.showAnswer ? (
-				<div>
+				<AnswerContainer>
 					<p>{`You have mistaken by ${game.diff}`}</p>
 					<p>{`Right answer is ${game.character.height}`}</p>
 					<button onClick={() => game.reset()}>Again!</button>
-				</div>
+				</AnswerContainer>
 			) : null}
-		</div>
+		</AnswerContainer>
 	)
 }
 
